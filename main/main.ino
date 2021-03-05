04f37a18bf11dc8a3750909ddf582eb5a3ec7d28
coded-ragon <51366493+coded-ragon@users.noreply.github.com>
2021-03-05 14:27:21 -0600

    Add files via upload
    Added basic code

---
+++ main/main.ino
@@ -0,0 +1,29 @@
+//main.ino
+
+
+/**Objective 
+ * To create a usb device that can reset and/or deploy any version of chromeOS, MacOS, and Windows10 based OS.
+ * 
+ * Directive
+ * 
+ * 1. Create the base code within the setup function.
+ * 2. Create framework for a minumlist one LED menu control system.
+ * 3. Allow the ajustments of timed delayes between actions
+ * 4. Allow for ajustible fuctionality of mouse controlls.
+ * 5. Allow for ajustible finctionality of keyboard inputs. 
+ * 6. Expect future gpio menu selector utilzing binary decoding.
+ * 7. Don't Panic
+ * 8. 
+ * 9. DONT READ DIRECTIVE NINE
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
